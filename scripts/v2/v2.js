2f2d8a136655230763058715dc2bd631583e2695
smakalande <smakalande@virtusa.com>
2018-08-23 18:21:19 +0530

    Issues fixed

--- scripts/v2/v2.js
+++ scripts/v2/v2.js
@@ -1,9 +1,8 @@
 class Agent {
   constructor(opts) {
     // Initialize options
-    this.isActive = true;
-    this.age = opts.age || 0;
-    this.position = opts.position;
+    this.isActive = opts.isActive !== undefined ? opts.isActive : true;
+    this.age = opts.age !== undefined ? opts.age : 0;
   }
   step(entities) {}
   mate(partner) {}
@@ -19,15 +18,18 @@ class Simulation {
   }
   init(opts) {
     // Initialize options
-    this.matingEntityLimit = opts.matingEntityLimit || 50;
-    this.matingRate = opts.matingRate || 0.1;
-    this.repopulate = opts.repopulate || [];
-    this.initialEntities = opts.initialEntities || [];
+    this.matingEntityLimit =
+      opts.matingEntityLimit !== undefined ? opts.matingEntityLimit : 50;
+    this.matingRate = opts.matingRate !== undefined ? opts.matingRate : 0.1;
+    this.repopulate = opts.repopulate !== undefined ? opts.repopulate : [];
+    this.initialEntities =
+      opts.initialEntities !== undefined ? opts.initialEntities : [];
     this.selectMate =
-      opts.selectMate ||
-      ((entities, entity) => {
-        entities[0];
-      });
+      opts.selectMate !== undefined
+        ? opts.selectMate
+        : (entities, entity) => {
+            entities[0];
+          };
 
     this.entities = [];
     this.initialEntities.forEach(({ entity, count, opts }) => {
@@ -41,37 +43,36 @@ class Simulation {
       // Step
       e.step && e.step(this.entities);
       // Mate
-      if(e.mate &&
-        this.entities.filter(e => e.mate).length < this.matingEntityLimit &&
-        Math.random(1) < this.matingRate) {
-          this.entities.push(e.mate(this.selectMate(this.entities, e)));
-      }
+      e.mate &&
+        this.entities.filter(e => e && e.mate).length <
+          this.matingEntityLimit &&
+        Math.random(1) < this.matingRate &&
+        this.entities.push(e.mate(this.selectMate(this.entities, e)));
       // Render
       renderer && renderer.render(e, this.entities);
     });
     // Clean
-    this.entities = this.entities.filter(e => e.isActive);
+    this.entities = this.entities.filter(e => e && e.isActive);
     // Repopulate
     this.repopulate.forEach(getNewEnities => {
       this.entities.concat(getNewEnities());
     });
   }
-  getStats() {
-    return {
-      entities: this.entities
-    };
+  printStats() {
+    console.log(this.entities);
   }
 }
 
 const opts = {
   initialEntities: [
-    { entity: Agent, count: 5, opts: {} },
-    { entity: Agent, count: 3, opts: {} }
+    { entity: Agent, count: 2, opts: {age: 2}},
+    { entity: Agent, count: 5, opts: {age: 5}}
   ]
 };
 
 const sim = new Simulation();
 const renderer = new Renderer();
+
 sim.init(opts);
 sim.step(renderer);
-console.log(sim.getStats());
+sim.printStats();
