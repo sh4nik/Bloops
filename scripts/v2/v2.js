7293912aeeeb7f2e010dd15d0ead0febd68b418b
Shanik Makalande <sh4nik@gmail.com>
2019-01-28 09:23:39 +0530

    Updated

--- scripts/v2/v2.js
+++ scripts/v2/v2.js
@@ -5,14 +5,14 @@ class EntityProcessor {
     this.postStep = opts.postStep;
     this.entities = [];
   }
-  step({ renderOpts }) {
+  step({ renderer }) {
     if (this.preStep) this.preStep(this.entities);
     this.entities = [...this.entities, ...this.incubator];
     this.incubator = [];
     this.entities = this.entities.filter(e => e.isActive);
     this.entities.forEach(e => {
       if (e.step) e.step(this.entities, this.entitiesToInject);
-      if (renderOpts && e.render) e.render(this.entities, renderOpts);
+      if (renderer && e.render) e.render(this.entities, renderer);
     });
     if (this.postStep) this.postStep(this.entities);
   }
@@ -28,23 +28,25 @@ class EntityProcessor {
 }
 
 class Simulation {
-  constructor({ canvas, entityConfig }) {
+  constructor({ canvas, entityConfig, framerate }) {
     this.ep = new EntityProcessor({
       entities: EntityProcessor.produceEntities(entityConfig),
       preStep: function (entities) { },
       postStep: function (entities) { }
     });
-    this.stage = new createjs.StageGL(canvas,{ antialias: true });
+    this.stage = new createjs.StageGL(canvas, { antialias: true });
+    createjs.Ticker.framerate = framerate;
   }
   render() {
-    this.stage.setClearColor("#2299aa");
+    this.stage.setClearColor('#2299aa');
   }
   run() {
     this.render();
-    createjs.Ticker.addEventListener("tick", () => {
-      const renderOpts = { stage: this.stage };
-      this.ep.step({ renderOpts });
+    createjs.Ticker.addEventListener('tick', () => {
+      const renderer = { stage: this.stage };
+      this.ep.step({ renderer });
       this.stage.update();
+      console.log(createjs.Ticker.getMeasuredFPS());
     });
   }
 }
@@ -54,23 +56,23 @@ class Agent {
     this.isActive = isActive;
     this.age = age;
     this.x = 0;
-    this.y = 0;
+    this.y = 60;
   }
   step(entities, incubator) {
-    this.x += 1;
-    this.y += 1;
+    this.x += 3;
     this.age += 1;
   }
-  render(entities, renderOpts) {
-    let r = 20;
-    if(renderOpts && !this.circle) {
+  render(entities, renderer) {
+    let r = 30;
+    if (renderer && !this.circle) {
       this.circle = new createjs.Shape();
-      renderOpts.stage.addChild(this.circle);
+      renderer.stage.addChild(this.circle);
       this.circle.graphics.beginFill('#333').drawCircle(0, 0, r);
     }
-    this.circle.cache(-r,-r, r*2,r*2);
+    this.circle.cache(-r, -r, r * 2, r * 2);
     this.circle.x = this.x;
     this.circle.y = this.y;
+    if (this.x > renderer.stage.canvas.width) { this.x = 0; }
   }
 }
 
@@ -78,12 +80,13 @@ class Food {
   constructor({ isActive = true }) {
     this.isActive = isActive;
   }
-  render(entities, renderOpts) { }
+  render(entities, renderer) { }
 }
 
 function init() {
   const sim = new Simulation({
     canvas: 'main-canvas',
+    framerate: 60,
     entityConfig: [
       { Entity: Agent, count: 2, opts: { age: 2 } },
       { Entity: Agent, count: 5, opts: { age: 5 } },
